c5a819abd78dd241b7a1ce05594f4d16afa2b807
Вилков Виталий <takao.nakamuro@yandex.ru>
2024-11-02 15:35:01 +0300

    Собрал в одно целое весь функционал для главной страницы. create_response возвращает json файл с необходимыми данными

--- src/views.py
+++ src/views.py
@@ -1,27 +1,35 @@
+import json
 from src.utils import get_greeting, calculate_card_expenses, get_top_transactions
-from src.external_api import get_currency_rates
+from src.external_api import get_currency_rates, get_stock_prices
 
 
 def create_response(start_date):
     greeting = get_greeting()
-
     # Получаем данные
-    card_expenses = calculate_card_expenses(start_date)  # ваши карты с расходами
-    top_transactions = get_top_transactions(start_date)  # ваши топ-5 транзакций
+    card_expenses = calculate_card_expenses(start_date)  # карты с расходами
     currency_rates = get_currency_rates()
+    top_transactions = get_top_transactions(start_date)  # топ-5 транзакций
+
+    if not currency_rates:
+        print("Не удалось получить курсы валют.")
+        currency_rates = []  # Установим пустой список, чтобы избежать ошибок ниже
+
+    stock_prices = get_stock_prices()
 
     # Формируем ответ без дублирования ключа
     response = {
         "greeting": greeting,
-        "cards": card_expenses,  # Здесь cards - это список словарей с картами
-        "top_transactions": top_transactions,  # Топ-5 транзакций
-        "currency_rates": currency_rates
+        "cards": card_expenses,
+        "top_transactions": top_transactions,
+        "currency_rates": currency_rates,
+        "stock_prices": stock_prices
     }
-
-    return response
+    json_response = json.dumps(response, ensure_ascii=False, indent=4)
+    return json_response
 
 
 # Пример вызова функции
 start_date = "22-12-2021"  # укажите нужную дату
 print(create_response(start_date))
+
 
